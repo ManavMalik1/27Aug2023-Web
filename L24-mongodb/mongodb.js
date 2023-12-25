b6ac8d104cc54bc18ca911716c78b22e42fee1d0
ManavMalik1 <manavmalikb@gmail.com>
2023-12-25 12:28:50 +0530

    MongoDB

---
+++ L24-mongodb/mongodb.js
@@ -0,0 +1,15 @@
+teacher({
+    id:ObjectId('65890c67398e41625aa38bb1'),
+    name:'monu',
+    subject:'java'
+},
+{
+    id:ObjectId('65890c67398e41625aa38bb2'),
+    name:'monu',
+    subject:'java'
+},
+{
+    id:ObjectId('65890c67398e41625aa38bb3'),
+    name:'monu',
+    subject:'java'
+})
